4f7f8fedec2d40166e2b3c3282f2b0c7f22a0446
Chronos Ouroboros <doom2.fan@gmail.com>
2015-12-17 07:44:12 -0200

    AUGH. FUCK MAKE.

---
+++ C_Source/src/misc.c
@@ -0,0 +1,181 @@
+/*
+**  Se7evidas - A GZDoom mod
+**  Copyright (C) 2015  Chronos Ouroboros
+**
+**  This program is free software; you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation; either version 2 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License along
+**  with this program; if not, write to the Free Software Foundation, Inc.,
+**  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include "includes.h"
+#include "misc.h"
+
+Script_C void S7_SpeedScript ENTER () {
+    // Not needed or desired in TitleMaps.
+    if (GameType () == GAME_TITLE_MAP)
+        return;
+    
+    int health;
+    
+    while (true) {
+        health = GetActorProperty (0, APROP_Health);
+        
+        if (health <= 20)
+            GiveInventory (s"S7_Dying", 9999999);
+        else
+            TakeInventory (s"S7_Dying", 9999999);
+        
+        Delay(1);
+    }
+}
+
+Script_C void S7_WaterScript ENTER () {
+    // Not needed or desired in TitleMaps.
+    if (GameType () == GAME_TITLE_MAP)
+        return;
+    
+    while (true) {
+        if (GetActorProperty(0, APROP_Waterlevel) > 2) // if underwater...
+            GiveInventory (s"S7_IsUnderwater", 1); // give S7_IsUnderwater
+
+        else if (GetActorProperty(0, APROP_Waterlevel) <= 2) // if not underwater
+            TakeInventory (s"S7_IsUnderwater", 1); // take S7_IsUnderwater
+            
+        SetInventory (s"S7_AirTime", GetAirSupply (PLN));
+        Delay (1);
+    }
+}
+
+Script_C void S7_Keys ENTER () {
+    // Not needed or desired in TitleMaps.
+    if (GameType () == GAME_TITLE_MAP)
+        return;
+    
+    while (true) {
+        if (KeyPressed (BT_RELOAD))
+            UseInventory (s"S7_ReloadKey");
+        
+        Delay (1);
+    }
+}
+
+#ifndef DISABLEBDCCOMPAT
+Script_C void S7_BrutalDoomCompatibility OPEN () {
+    // Not needed or desired in Titlemaps.
+    if (GameType () == GAME_TITLE_MAP)
+        return;
+    
+    bool ACTIVATE = 0;
+    bool tid = UnusedTID (-37000, -47000);
+    int delayer = 0;
+    
+    while (true) {
+        if (Spawn (s"Brutal_Blood", 0.0k, 0.0k, 0.0k, tid) ||
+            Spawn (s"BrutalPistol", 0.0k, 0.0k, 0.0k, tid)) {
+            Thing_Remove (tid);
+            ACTIVATE = 1;
+        }
+        
+        if (ACTIVATE) {
+            int randomizer = Random (0, 2);
+            int randomizer2 = 0;
+            int i = 0;
+            int actionCount = 0;
+            
+            if (delayer == 0) {
+                if (randomizer == 0) { // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                    delayer = 35 * Random (10, 20);
+                    for (i = 0; i < Random (5, 30); i++) {
+                        Spawn (s"GETTHATSHITOUTTAHERE", GetActorX (0) + RandomFixed (-200.0k, 200.0k), GetActorY (0) + RandomFixed (-200.0k, 200.0k), GetActorZ (0), tid);
+                        Thing_Hate (tid, 0, 2);
+                        Thing_ChangeTID (tid, 0);
+                        NoiseAlert (0, 0);
+                    }
+                } else if (randomizer == 1) { // Fuck sector colours up
+                    delayer = 35 * Random (10, 20);
+                    for (i = 0; i < 32767; i++) {
+                        Sector_SetColor (i, Random (0, 255), Random (0, 255), Random (0, 255), 0);
+                        actionCount++;
+                        if (actionCount >= 10000) {
+                            Delay (1);
+                            actionCount = 0;
+                        }
+                    }
+                } else if (randomizer == 2) { // Fuck flat rotations up
+                    delayer = 35 * Random (10, 20);
+                    for (i = 0; i < 32767; i++) {
+                        Sector_SetRotation (i, Random (0, 356), Random (0, 356));
+                        actionCount++;
+                        if (actionCount >= 10000) {
+                            Delay (1);
+                            actionCount = 0;
+                        }
+                    }
+                }
+                
+                if (GetLevelInfo (LEVELINFO_KILLED_MONSTERS) >= GetLevelInfo (LEVELINFO_TOTAL_MONSTERS) ||
+                    GetLevelInfo (LEVELINFO_FOUND_SECRETS)   >= GetLevelInfo (LEVELINFO_TOTAL_SECRETS)  ||
+                    GetLevelInfo (LEVELINFO_FOUND_ITEMS)     >= GetLevelInfo (LEVELINFO_TOTAL_ITEMS)) {
+                    for (i = 0; i < 10; i++) {
+                        Spawn (s"GETTHATSHITOUTTAHERE", GetActorX (0) + RandomFixed (-200.0k, 200.0k), GetActorY (0) + RandomFixed (-200.0k, 200.0k), GetActorZ (0), tid);
+                        Thing_Hate (tid, 0, 2);
+                        Thing_ChangeTID(tid, 0);
+                        NoiseAlert (0, 0);
+                    }
+                }
+            }
+            
+            i = 0;
+            randomizer = 0;
+            randomizer2 = 0;
+        }
+        
+        Delay (1);
+        if (delayer > 0)
+            delayer--;
+        if (delayer < 0)
+            delayer = 0;
+    }
+}
+#endif
+
+#ifdef DEBUG
+Script_C void S7_DebugVelocity () {
+    accum x = 0, y = 0, z = 0,
+        angle = 0, speed = 0;
+    while (true) {
+        x = GetActorVelX (0);
+        y = GetActorVelY (0);
+        z = GetActorVelZ (0);
+        speed = x * x + y * y + z * z;
+        Print ("Velocity: %k\n", FixedSqrt (speed));
+        
+        Delay (1);
+    }
+}
+
+Script_C void S7_DebugVelocityInKmH () {
+    accum x = 0, y = 0, z = 0,
+        speed = 0, speed2;
+    while (true) {
+        x = GetActorVelX (0);
+        y = GetActorVelY (0);
+        z = GetActorVelZ (0);
+        speed = x * x + y * y + z * z;
+        speed2 = 3.6k * ((FixedSqrt (speed) * 35.0k) / 64.0k);
+        Print ("Velocity: %k km/h\n", speed2);
+        
+        Delay (1);
+    }
+}
+#endif
---
+++ C_Source/src/weapon_stuff.c
@@ -0,0 +1,126 @@
+/*
+**  Se7evidas - A GZDoom mod
+**  Copyright (C) 2015  Chronos Ouroboros
+**
+**  This program is free software; you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation; either version 2 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License along
+**  with this program; if not, write to the Free Software Foundation, Inc.,
+**  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include "includes.h"
+#include "weapon_stuff.h"
+
+string WeaponName [] = {
+    s"S7_AMG",
+    s"S7_PrettyShootyIonCannonGun",
+    s"S7_Shotgun",
+    s"S7_Raptor",
+    s"S7_Revolver",
+    s"S7_TEC9"
+};
+
+string DummyWeapons [] = {
+    s"S7_NullWeapon",
+    s"S7_SprintWeapon",
+    s"S7_QuickMelee"
+};
+
+int GlobalVar LastWeapon [MAX_PLAYERS];
+
+int GetWeaponName () {
+    for (int x = 0; x < ArraySize (DummyWeapons); x++)
+        if (CheckWeapon (DummyWeapons [x])) 
+            return -1;
+    
+    for (int y = 0; y < ArraySize (WeaponName); y++)
+        if (CheckWeapon (WeaponName [y]))
+            return y;
+    
+    return -1;
+}
+
+void ChangeLastWeapon (bool mode) {
+    int weaponNumber = 0;
+    if (mode) {
+        weaponNumber = LastWeapon [PLN];
+        if (weaponNumber < 0 || weaponNumber > ArraySize (WeaponName) - 1)
+            return;
+        SetWeapon (WeaponName [weaponNumber]);
+    } else {
+        weaponNumber = GetWeaponName ();
+        if (weaponNumber < 0 || weaponNumber > ArraySize (WeaponName) - 1)
+            return;
+        else
+            LastWeapon [PLN] = weaponNumber;
+    }
+}
+
+void DisableWeapon (string meh, string blah) {
+    if (CheckWeapon (meh)) {
+        TakeInventory (blah, 99999);
+        ChangeLastWeapon (1);
+        return;
+    }
+    GiveInventory (meh, 1);
+    SetWeapon (meh);
+    ChangeLastWeapon (0);
+}
+
+// Scripts
+// SynthFire stuff
+Script_C void S7_SynthFire () {
+    while (TRUE) {
+        if (!CheckInventory (s"S7_SynthFireActive"))
+            return;
+        
+        if (KeyDown (BT_ATTACK) && !CheckInventory (s"S7_SynthFireLeft"))
+            GiveInventory (s"S7_SynthFireLeft", 1);
+        
+        if (KeyDown (BT_ALTATTACK) && !CheckInventory (s"S7_SynthFireRight"))
+            GiveInventory (s"S7_SynthFireRight", 1);
+        
+        Delay (1);
+        
+        if (KeyUp (BT_ATTACK) && CheckInventory (s"S7_SynthFireLeft"))
+            TakeInventory (s"S7_SynthFireLeft", 1);
+        
+        if (KeyUp (BT_ALTATTACK) && CheckInventory (s"S7_SynthFireRight"))
+            TakeInventory (s"S7_SynthFireRight", 1);
+    }
+}
+
+Script_C int S7_SynthFireAllowChange () {
+    if (!CheckInventory (s"S7_SynthFireRightReloading") || CheckInventory (s"S7_SynthFireLeftReloading"))
+        return 1;
+    else
+        return 0;
+}
+
+Script_C void S7_QuickMelee () {
+    DisableWeapon (s"S7_QuickMelee", s"None");
+}
+
+Script_C int S7_GetAutoReloading () {
+    if (GetUserCVar (PLN, s"S7_AutoReloading") == false)
+        return 0;
+    else
+        return 1;
+}
+
+Script_C void S7_RecoilPitch (accum offset) { // Called like this in code: TNT1 A 0 namedExecuteAlways ("S7_RecoilPitch", 0, 0.5 * 65535)
+    accum oldPitch = GetActorPitch (0);
+    accum scaledOffset = ScaleValueAccum (offset, -90.0k, 90.0k, -0.25k, 0.25k);
+    accum newPitch = ClampAccum (oldPitch - scaledOffset, -0.25k, 0.25k);
+    
+    SetActorPitch (0, newPitch);
+}
